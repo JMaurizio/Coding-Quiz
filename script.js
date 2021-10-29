34627f32f2c80d7e8b90588ff986c31d15ef9103
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2021-10-29 15:19:44 -0500

    timer added

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
-var quest = [
-    "Commonly used data types do not include.",
-    "The condition of an if/else statement is enclosed within _______.",
-    "Arrays in Javascript can be used to store _______.",
-    "String values must be enclosed within ________ whe being assigned to variables.",
-    "A useful tool used during development for printing content to the debugger is."
-]
-var answer0 = ["strings","booleans","alerts","numbers"]
-var answer1 = ["quotes","curly brackets","parenthesis","square brackets"]
-var answer2 = ["numbers and strings","other arrays","booleans","all of the above"]
-var answer3 = ["commas","curly brackets", "quotes","parenthesis"]
-var answer4 = ["Javascript","terminal/bash","for loops","console.log"]
+var qn = [
+    {
+    question: "Commonly used data types do not include.",
+    answers:["strings","booleans","alerts","numbers"],
+    correct:"alerts"
+    },
+    {
+    question: "The condition of an if/else statement is enclosed within _______.",
+    answers:["quotes","curly brackets","parenthesis","square brackets"],
+    correct:"parenthesis"
+    },
+    {
+    question: "Arrays in Javascript can be used to store _______.",
+    answers:["numbers and strings","other arrays","booleans","all of the above"],
+    correct:"all of the above"
+    },
+    {
+    question: "String values must be enclosed within ________ whe being assigned to variables.",
+    answers:["commas","curly brackets", "quotes","parenthesis"],
+    correct:"quotes"
+    },
+    {
+    question: "A useful tool used during development for printing content to the debugger is.",
+    answers:["Javascript","terminal/bash","for loops","console.log"],
+    correct:"console.log"
+    }
+];
+
+var start = document.querySelector("#start-btn")
+var timeLeft = document.querySelector("#timer")
+var seconds = 75
+
+start.addEventListener("click", function() {
+    if(seconds === 75) {
+        setInterval(function() {
+            seconds--;
+            timeLeft.textContent = "Time left:" + seconds;
+            
+            if(seconds <= 0) {
+                clearInterval(timeLeft);
+                timeLeft.textContent = "Out of time!";
+            }
+        }, 1000);
+    };
+});
