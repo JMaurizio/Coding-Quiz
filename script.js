46e95baaa50377a652b91eb874e88cc4d52030b4
JMaurizio <90101715+JMaurizio@users.noreply.github.com>
2021-10-29 14:19:12 -0500

    html setup js variables added

---
+++ script.js
@@ -0,0 +1,12 @@
+var quest = [
+    "Commonly used data types do not include.",
+    "The condition of an if/else statement is enclosed within _______.",
+    "Arrays in Javascript can be used to store _______.",
+    "String values must be enclosed within ________ whe being assigned to variables.",
+    "A useful tool used during development for printing content to the debugger is."
+]
+var answer0 = ["strings","booleans","alerts","numbers"]
+var answer1 = ["quotes","curly brackets","parenthesis","square brackets"]
+var answer2 = ["numbers and strings","other arrays","booleans","all of the above"]
+var answer3 = ["commas","curly brackets", "quotes","parenthesis"]
+var answer4 = ["Javascript","terminal/bash","for loops","console.log"]
